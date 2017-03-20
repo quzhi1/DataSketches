9b46d79208e24c6eef7f49f1a72d51b621e236a3
Zhi Qu <zhiqu@yahoo-inc.com>
2017-03-20 11:42:16 -0700

    Copy and paste, but not working

--- src/main/java/com/quzhi/datasketches/Main.java
+++ src/main/java/com/quzhi/datasketches/Main.java
@@ -1,21 +1,26 @@
 package com.quzhi.datasketches;
 
-import com.yahoo.sketches.memory.NativeMemory;
+import com.yahoo.memory.NativeMemory;
+import com.yahoo.sketches.ArrayOfStringsSerDe;
+import com.yahoo.sketches.frequencies.ErrorType;
+import com.yahoo.sketches.frequencies.ItemsSketch;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+
 
 /**
  * Created by zhiqu on 3/17/17.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // simplified file operations and no error handling for clarity
 
         // this section generates two sketches and serializes them into files
         {
-            FrequentItemsSketch<String> sketch1 = new FrequentItemsSketch<String>(64);
+            ItemsSketch<String> sketch1 = new ItemsSketch<String>(64);
             sketch1.update("a");
             sketch1.update("a");
             sketch1.update("b");
@@ -27,7 +32,7 @@ public class Main {
             out1.write(sketch1.toByteArray(new ArrayOfStringsSerDe()));
             out1.close();
 
-            FrequentItemsSketch<String> sketch2 = new FrequentItemsSketch<String>(64);
+            ItemsSketch<String> sketch2 = new ItemsSketch<String>(64);
             sketch2.update("e");
             sketch2.update("a");
             sketch2.update("f");
@@ -47,28 +52,28 @@ public class Main {
             byte[] bytes1 = new byte[in1.available()];
             in1.read(bytes1);
             in1.close();
-            FrequentItemsSketch<String>
+            ItemsSketch<String>
                 sketch1 =
-                FrequentItemsSketch.getInstance(new NativeMemory(bytes1), new ArrayOfStringsSerDe());
+                ItemsSketch.getInstance(new NativeMemory(bytes1), new ArrayOfStringsSerDe());
 
             FileInputStream in2 = new FileInputStream(new File("FrequentStringsSketch2.bin"));
             byte[] bytes2 = new byte[in2.available()];
             in2.read(bytes2);
             in2.close();
-            FrequentItemsSketch<String>
+            ItemsSketch<String>
                 sketch2 =
-                FrequentItemsSketch.getInstance(new NativeMemory(bytes2), new ArrayOfStringsSerDe());
+                ItemsSketch.getInstance(new NativeMemory(bytes2), new ArrayOfStringsSerDe());
 
             // we could merge sketch2 into sketch1 or the other way around
             // this is an example of using a new sketch as a union and keeping the original sketches intact
-            FrequentItemsSketch<String> union = new FrequentItemsSketch<String>(64);
+            ItemsSketch<String> union = new ItemsSketch<String>(64);
             union.merge(sketch1);
             union.merge(sketch2);
 
-            FrequentItemsSketch.Row<String>[] items = union.getFrequentItems(ErrorType.NO_FALSE_POSITIVES);
+            ItemsSketch.Row<String>[] items = union.getFrequentItems(ErrorType.NO_FALSE_POSITIVES);
             System.out.println("Frequent items: " + items.length);
-            System.out.println(FrequentItemsSketch.Row.getRowHeader());
-            for (FrequentItemsSketch.Row<String> row : items) {
+            System.out.println(ItemsSketch.Row.getRowHeader());
+            for (ItemsSketch.Row<String> row : items) {
                 System.out.println(row.toString());
             }
         }
